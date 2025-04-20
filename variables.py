a85654c3f80a7d3d173ad97009b069e49d069f04
123456-manvi <103314650+123456-manvi@users.noreply.github.com>
2025-04-20 20:14:16 +0530

    Create variables-datatype.py

---
+++ variables.py
@@ -0,0 +1,65 @@
+p,q,r=11,12,13
+print(p,q,r)
+
+# pascal case
+MyName="Madhav"
+
+# camel case
+myName="Madhav"
+
+# flat case
+myname="Madhav"
+
+# snake case
+my_name="Madhav"
+
+
+# data type.........
+# 1 interger
+a=1
+b=1
+print(a+b)
+
+print(type(a))
+print(type(b))
+
+
+# 2 string
+c="1"
+d="1"
+print(c+d)
+print(type(c))
+
+
+# 3 complex
+a3=comples(3,5)
+print(type(a3))
+
+
+# sequence
+b1="madhav"
+print(type(b1))
+
+# list
+b2=[1,4,7,"madhav"]
+print(type(b2))
+
+# tuple
+b3=(1,4,7,"madhav")
+print(type(b3))
+
+# dictionary......
+my_dictionary={'name' : 'rishabh' , 'age' : 26, 'city' : 'Prayagraj'}
+print(type(my_dictionary))
+
+# sets
+my_sets={1,4,7,"madhav"}
+print(type(my_sets))
+
+# boolean
+bool1=True
+print(type(bool1))
+
+# binary
+# bytes, bytearray, memoryview
+print(type(byte1))
